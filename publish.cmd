b9fe369211e42c659628df898d0bee0df7ca6cfa
Claus Matzinger <claus.matzinger+kb@gmail.com>
2017-12-13 10:29:16 +0100

    initial

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.LuisBot.sln -p:DeployOnBuild=true -p:PublishProfile=santashelper-Web-Deploy.pubxml -p:Password=6vb0eYsLpfse2Mg36siSm5oxoCdJymbivfkf6dw8wuK3ESkLmkLYTXxh7res
+
